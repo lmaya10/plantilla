e28d86e15c7c1d63c0afa23613baea7548f87f8b
Javier Santiago Barbosa Castillo <js.barbosa11@uniandes.edu.co>
2019-09-17 11:49:57 -0500

    Funcionan los datos pero no estan lindos

--- front/src/Datos.js
+++ front/src/Datos.js
@@ -11,35 +11,61 @@ class Datos extends Component{
     this.traerInfo = this.traerInfo.bind(this);
   }
 
-
-  /*sort_by_key(array, key)
-  {
-    return array.sort(function(a, b)
-    {
-      let x = a[key];
-      let y = b[key];
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-  }*/
-
+  renderDatos() {
+    return this.state.datos.map(t => (
+      <div>
+        <p>{t.id + " " + t.visitas + " " +t.url.url }</p>
+      </div>
+    ));
+  }
+  
   traerInfo(){
 
     fetch("https://www.datos.gov.co/resource/gc9e-9mzf.json")
       .then(response => response.json())
       .then(response => {
-        console.log("Datos", response[0]);
+        console.log("Datos", response);
         //this.setState({datos: response})
+        let datos = response;
+        let filtrados = [];
+        for(let cadauno in datos)
+        {
+
+          if(datos[cadauno].visitas <  3)
+          {
+            filtrados.push(datos[cadauno]);
+          }
+        }
+
+        console.log("filtrados",filtrados);
+        // ordenar de menor a mayor
+
+        //let ordenados=filtrados.sort((a,b) => (a.visitas > b.visitas) ? 1 : ((b.visitas> a.visitas) ? -1 : 0));
+
+        //ordenar de mayor a menor
+        let ordenados=filtrados.sort((a,b) => (a.visitas < b.visitas) ? 1 : ((b.visitas< a.visitas) ? -1 : 0));
+
+        this.setState({datos:filtrados});
+
     })
   }
-  ordenarlos() {
-    let ordenados=[];
-  }
+
+//this.setState({operacion}, callback) si necesito llamar this.state inmediatamente.
 
 
   render () {
     return(
       <div> Hola aca irian los datos
-        <button type="submit" value="Search" onClick={this.traerInfo} />
+        <div> 
+          <input typ="text"></input>
+          <button type="button" onClick={this.traerInfo}>Traer datos </button>
+          <div>
+          {
+            this.renderDatos()
+            //this.state.datos.map(t => <p> {t.visitas} </p>)
+          }
+          </div>
+        </div>  
       </div>
 
       );
