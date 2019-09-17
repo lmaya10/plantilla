2e4b3faa129eff4b77e0d8008cc681494bca229b
lmaya10 <l.maya10@uniandes.edu.co>
2019-09-17 13:27:18 -0500

    Trae los datos del link del input

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <h1> Visualizacion Datos Datos.gov.co</h1>
       <Datos></Datos>
+
     </div>
-  )
+    )
 }
 
 export default App;
--- src/Datos.js
+++ src/Datos.js
@@ -1,12 +1,14 @@
 
 import React, {Component} from "react";
+import navio from "navio";
 
 class Datos extends Component{
 
   constructor(props){
     super(props);
     this.state={
-      'datos': []
+      "urlBuscar": "",
+      "datos": []
     }
     this.traerInfo = this.traerInfo.bind(this);
   }
@@ -14,55 +16,36 @@ class Datos extends Component{
   renderDatos() {
     return this.state.datos.map(t => (
       <div>
-        <p>{t.id + " " + t.visitas + " " +t.url.url }</p>
+        <p>{t.region }</p>
       </div>
     ));
   }
+
+  onChange( ){
+    return this.setState({urlBuscar:this.linkInput.value});
+  }
   
   traerInfo(){
-
-    fetch("https://www.datos.gov.co/resource/gc9e-9mzf.json")
+    return(
+    fetch(this.state.urlBuscar)
       .then(response => response.json())
       .then(response => {
         console.log("Datos", response);
-        //this.setState({datos: response})
         let datos = response;
-        let filtrados = [];
-        for(let cadauno in datos)
-        {
+        this.setState({datos:response});
 
-          if(datos[cadauno].visitas <  3)
-          {
-            filtrados.push(datos[cadauno]);
-          }
-        }
-
-        console.log("filtrados",filtrados);
-        // ordenar de menor a mayor
-
-        //let ordenados=filtrados.sort((a,b) => (a.visitas > b.visitas) ? 1 : ((b.visitas> a.visitas) ? -1 : 0));
-
-        //ordenar de mayor a menor
-        let ordenados=filtrados.sort((a,b) => (a.visitas < b.visitas) ? 1 : ((b.visitas< a.visitas) ? -1 : 0));
-
-        this.setState({datos:filtrados});
-
-    })
+    }))
   }
-
-//this.setState({operacion}, callback) si necesito llamar this.state inmediatamente.
-
 
   render () {
     return(
-      <div> Hola aca irian los datos
+      <div> Ingrese el link de la información que desea consultar
         <div> 
-          <input typ="text"></input>
+          <input type="text" ref ={linkInput => this.linkInput = linkInput} onInput = {this.onChange.bind(this)}></input>
           <button type="button" onClick={this.traerInfo}>Traer datos </button>
           <div>
           {
             this.renderDatos()
-            //this.state.datos.map(t => <p> {t.visitas} </p>)
           }
           </div>
         </div>  
